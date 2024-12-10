b07199bd2d63e55ce8b3faa5740f135870c700c7
gustavofcamposs <gustavofurtado534@gmail.com>
2024-12-10 10:26:24 -0300

    Pequenos ajustes

--- ToyHouse Front-end/Script/Home/headerHamburguer.js
+++ ToyHouse Front-end/Script/Home/headerHamburguer.js
@@ -4,6 +4,9 @@ function clickMenu() {
     e 'estilos' usa window.getComputedStyle(menu) para pegar os estilos aplicados ao menu no momento da chamada.*/
 
 
+    let submenu = document.getElementById('subMenu'); /*Preciso deixar o SUBMENU none quando estiver aberto o Menu Hamburguer*/
+
+
     let menu = document.getElementById('menu-hamburguer');
     var estilos = window.getComputedStyle(menu);
 
@@ -12,8 +15,10 @@ function clickMenu() {
     if (estilos.display === 'none') {
 
         menu.style.display = "flex"; //Torna Visivel
+        submenu.style.display = "none";
     } else {
         menu.style.display = "none";
+        submenu.style.display = "flex";
     }
 
     console.log('FLEX:' + estilos.display);
